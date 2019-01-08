1089af06690cdba7067f013ff7c9598b7f1036ef
Yves Riedener <yves.riedener@uay.ch>
2019-01-08 17:19:42 +0100

    added cleanup

---
+++ cleanup.js
@@ -0,0 +1,31 @@
+// Credits: https://stackoverflow.com/a/21947851/3359418
+
+// Object to capture process exits and call app specific cleanup function
+function noOp() {
+}
+
+exports.Cleanup = function Cleanup(callback) {
+
+    // attach user callback to the process event emitter
+    // if no callback, it will still exit gracefully on Ctrl-C
+    callback = callback || noOp;
+    process.on('cleanup', callback);
+
+    // do app specific cleaning before exiting
+    process.on('exit', function () {
+        process.emit('cleanup');
+    });
+
+    // catch ctrl+c event and exit normally
+    process.on('SIGINT', function () {
+        console.log('Ctrl-C...');
+        process.exit(2);
+    });
+
+    //catch uncaught exceptions, trace, then exit normally
+    process.on('uncaughtException', function (e) {
+        console.error('Uncaught Exception...');
+        console.error(e.stack);
+        process.exit(99);
+    });
+};
--- index.js
+++ index.js
@@ -8,13 +8,18 @@ logger.debug('loading');
 
 const SenseHat = require('node-sense-hat');
 const Shapes = require('./shapes.js');
+const Cleanup = require('./cleanup.js').Cleanup;
 
 // MAIN CODE
 
 const Main = function () {
     const matrix = SenseHat.Leds;
 
+    Cleanup(function () {
+        logger.debug('done');
 
+        matrix.clear([0, 0, 0]);
+    });
 
     matrix.setPixels(Shapes.square(Shapes.colors.blue));
 
